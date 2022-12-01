b63f47dde6d04d68756fd5527ce5f4b9a2ab737d
teoyuqi <teo.yuqi@gmail.com>
2022-12-01 23:08:25 +0800

    Create base template

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react'
+
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Experience from './components/experience/Experience'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+
+const App = () => {
+  return (
+    <Fragment>
+      App
+      <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Contact/>
+    </Fragment>
+  )
+}
+
+export default App
