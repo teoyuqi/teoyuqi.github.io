4756dc6382612034bbdeccc8752b3697af479e60
teoyuqi <teo.yuqi@gmail.com>
2022-12-04 20:46:11 +0800

    Add base html skeleton

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -3,7 +3,50 @@ import './experience.css'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h2>Work and Volunteer experience</h2>
+      <div className='container experience__container'>
+        <article className='experience__item'>
+          {/* TikTok Logo */}
+          <h5>COMPANY NAME - role</h5>
+          <small>START DATE - END DATE</small>
+        </article>
+
+        <article className='experience__item'>
+          {/* DSO Logo */}
+          <h5>COMPANY NAME - role</h5>
+          <small>START DATE - END DATE</small>
+          <ul>
+            <li>Tasks handled</li>
+            <li>Tasks handled</li>
+          </ul>
+        </article>
+
+        <article className='experience__item'>
+          {/* NUS Logo */}
+          <h5>COMPANY NAME - role</h5>
+          <small>START DATE - END DATE</small>
+          <ul>
+            <li>Tasks handled</li>
+            <li>Tasks handled</li>
+          </ul>
+        </article>
+
+        <div className='container skills__container'>
+          <article className='skill__item'>
+            <h5>Fluent in</h5>
+            {/* Logos */}
+          </article>
+
+          <article className='skill__item'>
+            <h5>Also used</h5>
+            {/* Logos */}
+          </article>
+        </div>
+
+        <a href="#contact" className='btn btn-primary'>Let's talk!</a>
+      </div>
+    </section>
   )
 }
 
