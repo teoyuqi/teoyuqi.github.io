d96e88968c3195ffd8c425fa7a17c3b753ece13f
teoyuqi <teo.yuqi@gmail.com>
2022-12-07 21:55:22 +0800

    Add scroll animation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,12 +11,14 @@ const Header = (props) => {
   return (
     <header id='header'>
       <div className='container header__container'>
-        <h5>Hello I'm</h5>
-        <h1>Yu Qi</h1>
-        <h5 className='text-light'>Computer Science Undergraduate | NUS </h5>
-        <CallToAction/>
-        <div className='formal-portrait'>
-          <img src={formalPortrait} alt="me" />
+        <div className='header__content' ref={observeElement}>
+          <h5>Hello I'm</h5>
+          <h1>YU QI</h1>
+          <h5 className='text-light'>Software Engineer, Computer Science Undergraduate </h5>
+          <CallToAction/>
+          <div className='formal-portrait'>
+            <img src={formalPortrait} alt="me" />
+          </div>
         </div>
         <HeaderSocials/>
       </div>
