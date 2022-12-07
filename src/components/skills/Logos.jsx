c06dc5be0465fdd25002f7e19ec26b850c45edeb
teoyuqi <teo.yuqi@gmail.com>
2022-12-07 17:35:03 +0800

    Add programming languages experience

---
+++ src/components/skills/Logos.jsx
@@ -0,0 +1,65 @@
+// Icon links. 
+const logoIcons = {
+    arduino: {
+        name: "Arduino",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/arduino/arduino-original.svg"
+    },
+    bash: {
+        name: "Bash",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg"
+    },
+    c: {
+        name: "C",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg"
+    },
+    cpp: {
+        name: "C++",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg"
+    },
+    css: {
+        name: "CSS",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+    },
+    firebase: {
+        name: "Firebase",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg"
+    },
+    git: {
+        name: "Git",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
+    },
+    html: {
+        name: "HTML",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+    },
+    java: {
+        name: "Java",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg"
+    },
+    javascript: {
+        name: "Javascript",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+    },
+    python: {
+        name: "Python",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
+    },
+    rails: {
+        name: "Rails",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-original-wordmark.svg"
+    },
+    react: {
+        name: "React",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+    },
+    ruby: {
+        name: "Ruby",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original.svg"
+    },
+    xilinx: {
+        name: "Xilinx Vitis-AI",
+        icon: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Xilinx_logo.svg"
+    }
+};
+
+export {logoIcons};
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,28 +1,49 @@
 import React from 'react'
 
+import { logoIcons } from './Logos'
 import './skills.css'
 
 const Skills = () => {
+  // Generates block element contaning icon and its name.
+  const getLogoElement = (tool) => {
+    return (
+        <div className='skills__icon' id={tool}>
+          <img src={logoIcons[tool]["icon"]} alt={tool} className={`skills__icon-${tool}`}/>
+          <div>{logoIcons[tool]["name"]}</div>
+        </div>
+    )
+  };
+
+  // Block elements of tools I'm fluent in.
+  const toolsImFluentIn = [ "python", "java", "javascript", "html", "css", "react", "git" ].map(getLogoElement);
+ 
+  // Block elements of tools I've used before.
+  const toolsIveAlsoUsed = [ "c", "cpp", "bash", "rails", "ruby", "firebase", "arduino", "xilinx" ].map(getLogoElement);
+
   return (
     <section id='skills'>
       <h2>Skills</h2>
       <div className='container skills__container'>
 
           <div>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi cupiditate perferendis ab repudiandae ad similique explicabo iusto doloremque esse, corporis deleniti facere quod nisi possimus quam eligendi. Impedit, reiciendis unde.</p>
+            <p>Here are some of the programming languages and frameworks I have used in the past. Hit me up if I have the skillsets you are looking for!</p>
 
-            <a href="#contact" className='btn btn-primary'>Let's talk!</a>
+            <a href="#contact" className='btn btn-primary'>Hit Me Up!</a>
           </div>
 
           <div className='skills__cards'>
             <article className='skills__card'>
-              <h5>Fluent in</h5>
-              {/* Logos */}
+              <div className='skills__title'>Fluent in</div>
+              <div className='skills__icons'>
+                {toolsImFluentIn}
+              </div>
             </article>
 
             <article className='skills__card'>
-              <h5>Also used</h5>
-              {/* Logos */}
+              <div className='skills__title'>Also used</div>
+              <div className='skills__icons'>
+                {toolsIveAlsoUsed}
+              </div>
             </article>
           </div>
 
