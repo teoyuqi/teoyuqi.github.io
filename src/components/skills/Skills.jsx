bf27975b883dfc667d026d5b2db36ab271f9b8fd
teoyuqi <teo.yuqi@gmail.com>
2022-12-07 20:10:22 +0800

    Add animations to skills

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { logoIcons } from './Logos'
 import './skills.css'
 
 const Skills = () => {
+  const callback = (entries) => {
+      // Loop through all elements that are now in window.
+      entries.forEach((entry) => {
+        entry.target.classList.toggle('hide', !entry.isIntersecting);
+        entry.target.classList.toggle('show', entry.isIntersecting);
+    })};
+  const observer = new IntersectionObserver(callback);
+
   // Generates block element contaning icon and its name.
   const getLogoElement = (tool) => {
     return (
-        <div className='skills__icon' id={tool}>
+        <div className='skills__icon' key={tool}>
           <img src={logoIcons[tool]["icon"]} alt={tool} className={`skills__icon-${tool}`}/>
           <div>{logoIcons[tool]["name"]}</div>
         </div>
@@ -28,18 +36,18 @@ const Skills = () => {
           <div>
             <p>Here are some of the programming languages and frameworks I have used in the past. Hit me up if I have the skillsets you are looking for!</p>
 
-            <a href="#contact" className='btn btn-primary'>Hit Me Up!</a>
+            <a href="#contact" className='btn btn-primary'  ref={(element) => observer.observe(element)}>Hit Me Up!</a>
           </div>
 
           <div className='skills__cards'>
-            <article className='skills__card'>
+            <article className='skills__card'  ref={(element) => observer.observe(element)}>
               <div className='skills__title'>Fluent in</div>
               <div className='skills__icons'>
                 {toolsImFluentIn}
               </div>
             </article>
 
-            <article className='skills__card'>
+            <article className='skills__card' ref={(element) => observer.observe(element)}>
               <div className='skills__title'>Also used</div>
               <div className='skills__icons'>
                 {toolsIveAlsoUsed}
